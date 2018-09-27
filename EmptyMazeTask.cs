4836f86306341e9fe02c9aa0accccf2550340ea7
Farida <lithi-um@yandex.ru>
2018-09-27 12:53:20 +0500

    11

---
+++ EmptyMazeTask.cs
@@ -0,0 +1,31 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+
+            int steps = 2;
+            MoveDown(steps, robot, height);
+            MoveRight(steps, robot, width);
+        }
+
+        public static void MoveDown(int steps, Robot robot, int height)
+        {
+            for (; height != steps; height--)            
+            {
+                robot.MoveTo(Direction.Down);
+            }
+        }
+
+        public static void MoveRight(int steps, Robot robot, int width)
+        {
+            for (; (width != steps); width--)
+            {
+                robot.MoveTo(Direction.Right);
+            }
+
+        }
+
+    }
+}
